c4132963ccd7156e105fc552c95d11a60208ca93
Stephen Perry <stephen.perry@metcash.com>
2014-04-22 11:04:59 +1000

    New Web Services Client
    This project demonstrates how to call a web service via C#

---
+++ SampleRWSProject/sample.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SampleRWSProject
+{
+    partial class sample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAreYouThere = new System.Windows.Forms.Button();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.txtB2BAccount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAreYouThere
+            // 
+            this.btnAreYouThere.Location = new System.Drawing.Point(113, 79);
+            this.btnAreYouThere.Name = "btnAreYouThere";
+            this.btnAreYouThere.Size = new System.Drawing.Size(82, 23);
+            this.btnAreYouThere.TabIndex = 0;
+            this.btnAreYouThere.Text = "AreYouThere";
+            this.btnAreYouThere.UseVisualStyleBackColor = true;
+            this.btnAreYouThere.Click += new System.EventHandler(this.btnAreYouThere_Click);
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(95, 121);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOut.Size = new System.Drawing.Size(266, 73);
+            this.txtOut.TabIndex = 1;
+            // 
+            // txtB2BAccount
+            // 
+            this.txtB2BAccount.Location = new System.Drawing.Point(95, 26);
+            this.txtB2BAccount.Name = "txtB2BAccount";
+            this.txtB2BAccount.Size = new System.Drawing.Size(100, 20);
+            this.txtB2BAccount.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "B2BAccount";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(95, 53);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Response";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Submit Order";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 218);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtB2BAccount);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.btnAreYouThere);
+            this.Name = "sample";
+            this.Text = "Sample Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAreYouThere;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.TextBox txtB2BAccount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SampleRWSProject/sample.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SampleRWSProject
+{
+    public partial class sample : Form
+    {
+        public sample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAreYouThere_Click(object sender, EventArgs e)
+        {
+
+            try {
+
+                RWS.GAWebServicesClient rwsClient = new RWS.GAWebServicesClient();
+                RWS.Authentication authHeader = new RWS.Authentication();
+
+                authHeader.TpId = txtB2BAccount.Text;
+                authHeader.Password = txtPassword.Text;
+                authHeader.PillarId = "";
+                authHeader.StateCode = "";
+                authHeader.CustomerId = "";
+
+                RWS.areYouThere ru = new SampleRWSProject.RWS.areYouThere();
+                RWS.areYouThereResponse ruresp = new SampleRWSProject.RWS.areYouThereResponse();
+                
+                ruresp = rwsClient.areYouThere(authHeader, ru);
+                txtOut.Text = ruresp.response;
+
+            }
+            catch (Exception ex)
+            {
+                txtOut.Text = "Error: " + ex.Message;
+
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ORD.RetailWebServiceClient ordClient = new ORD.RetailWebServiceClient();
+            ORD.Authentication authHeader = new ORD.Authentication();
+
+            authHeader.B2BAccount = txtB2BAccount.Text;
+            authHeader.Password = txtPassword.Text;
+            authHeader.PillarId = "";
+            authHeader.StateCode = "";
+            authHeader.CustomerId = "";
+
+            ORD.placeWSOrder ord = new ORD.placeWSOrder();
+            ORD.placeWSOrderResponse ordresp = new ORD.placeWSOrderResponse();
+            ORD.placeWSOrderRequest ordreq = new ORD.placeWSOrderRequest();
+            
+            ord.OrderType = "PC01Order";
+            ord.WSOrder = "X1234567890 F05 F00123654 F27042011 FD  F01 F0000002118 F                    " 
+                        + Environment.NewLine + "0000000050148800078UD" 
+                        + Environment.NewLine + "0000000050512300060CD" 
+                        + Environment.NewLine + "Z 00004";
+            
+            ordreq.placeWSOrder = ord;
+            ordreq.Authentication = authHeader;
+            ordresp = ordClient.placeWSOrder(authHeader, ordreq.placeWSOrder);
+
+            txtOut.Text = ordresp.@return;
+
+        }
+    }
+}
